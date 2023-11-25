f9ae177e625f52c4f17eddbaf6406f625613de75
Bernhard Thoben <berni-the-bear@web.de>
2023-11-25 18:49:27 +0100

    Local assignments removed
    - This prevents error messages like this: "hardware/qcom-caf/msm8994/display/libhwcomposer/../common.mk: error: "hwcomposer.msm8994 (native:vendor) can not link against libmedia (native:platform)"."
    
    Change-Id: I87ed3a94cebf1a1756f195f13aa4695f1851ece0

--- libhwcomposer/Android.mk
+++ libhwcomposer/Android.mk
@@ -3,7 +3,6 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := hwcomposer.$(TARGET_BOARD_PLATFORM)
-LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
@@ -12,7 +11,7 @@ LOCAL_SHARED_LIBRARIES        := $(common_libs) libEGL liboverlay \
                                  libbinder libmedia
 
 ifeq ($(TARGET_USES_QCOM_BSP),true)
-LOCAL_SHARED_LIBRARIES += libhwui
+    LOCAL_SHARED_LIBRARIES += libhwui
 endif #TARGET_USES_QCOM_BSP
 
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\" -Wno-absolute-value \
